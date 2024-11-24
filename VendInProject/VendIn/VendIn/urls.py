1b4285e3764dcebf1b52241c7c4d624f162c393e
Ankita <ankitamohanty1919191@gmail.com>
2024-11-24 11:23:06 +0530

    Testing git commit.

--- VendInProject/VendIn/VendIn/urls.py
+++ VendInProject/VendIn/VendIn/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('VendInapp.urls')),  # Assuming your app name is 'VendInapp'
+    path('', include('VendInapp.urls')),  #  Assuming your app name is 'VendInapp'
 ]
